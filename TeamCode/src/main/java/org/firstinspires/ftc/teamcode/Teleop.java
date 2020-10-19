69a1a98b9059a11dd33707069a200d3556c85230
Tiberiw <amarietiberiu@yahoo.com>
2020-10-19 14:26:32 +0300

    Updated gradle file setting and created an empty teleop program

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Teleop.java
@@ -0,0 +1,25 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+public class Teleop extends LinearOpMode {
+
+    @Override
+    public void runOpMode()
+    {
+
+            waitForStart();
+
+            while(opModeIsActive())
+            {
+
+
+
+            }
+
+
+
+    }
+
+
+}
